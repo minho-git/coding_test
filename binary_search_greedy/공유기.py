3b5c5007be8431bc95bcf9f01629ceb86075d988
minho <kimminho001108@gmail.com>
2026-04-07 14:11:57 +0900

    4월 7일 코테

---
+++ binary_search_greedy/공유기.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+
+N, C = map(int, input().split())
+graph = []
+answer = 1
+
+for i in range(N):
+    graph.append(int(input()))
+
+graph.sort()
+
+start = 1
+end = graph[-1] - graph[0]
+
+while start <= end:
+    mid = (start + end) // 2
+    count = 1
+    tmp = mid
+
+    for i in range(1, N):
+        tmp -= (graph[i] - graph[i-1])
+
+        if tmp <= 0:
+            count += 1
+            tmp = mid
+
+    if count >= C:
+        start = mid + 1
+        answer = mid
+    else:
+        end = mid - 1
+
+print(answer)
---
+++ dp/LCS.py
@@ -0,0 +1,24 @@
+arr1 = list(input())
+arr2 = list(input())
+
+arr1.insert(0, "X")
+arr2.insert(0, "X")
+
+
+dp = [[0] * (len(arr2)) for _ in range(len(arr1))] # 행, 열 뒤집히는거 조심하자!
+
+for i in range(1, len(arr1)): # 길이가 이미 늘어났음!!
+    for j in range(1, len(arr2)):
+        if arr1[i] == arr2[j]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+
+answer = 0
+for i in range(1, len(dp)):
+    for j in range(1, len(dp[i])):
+        if answer < dp[i][j]:
+            answer = dp[i][j]
+
+print(answer)
